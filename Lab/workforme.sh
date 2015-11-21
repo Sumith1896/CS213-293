c1cb28561cd069d7762c3e18b5080c13b1de79ec
Sumith <sumith1896@gmail.com>
2015-11-21 19:32:30 +0530

    Added assignments of batch of 2014, removed sh script(me being lazy)

--- Lab/workforme.sh
+++
@@ -1,9 +0,0 @@
-for file in *.txt
-do
-    if [[ $file =~ [0-9] ]]; then
-    	newname="Assign"$(sed 's/[^ 0-9]*//g' <<< "$file")
-    	mkdir -p "${newname}"
-    	mv "${file}" "problem.txt"
-    	mv "problem.txt" "${newname}"/
-    fi
-done
